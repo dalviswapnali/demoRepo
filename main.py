9b50f1427e10989766ef469881b0935df701f79c
Dalvi Swapnali <dalviswapnali92@.in>
2024-06-19 20:31:51 +0530

    Added new checkpoint

--- main.py
+++ main.py
@@ -13,3 +13,17 @@ def about():
 @app.get("/Contact-us")
 def contact_us():
 	return {"email": "abc@gmail.com", "mob" : "1234567890"}
+
+@app.get("/login")
+def login_get(username : str, password : str):
+	if username == "dev" and password == "123456":
+	  return {"data": "This is login page", "mob" : "1234567890"}
+	else :
+		return {"data" : "Login failed incorrect username or password"}
+
+@app.post("/login")
+def login_post(username : str, password : str):
+	if username == "dev" and password == "123456":
+	  return {"data": "This is login page", "mob" : "1234567890"}
+	else :
+	  return {"data" : "Login failed incorrect password"}
